ff6de8a9ff140cb8a692bc151d34b21194c2e164
tungbachqm <tungbachqm@gmail.com>
2020-03-03 14:03:19 +0800

    This is full folders of SLR

---
+++ CityVoltexAssetTest/Assets/MyScripts/camEFf.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camEFf : MonoBehaviour
+{
+    public float waterlevel;
+    private bool isUnderwater;
+    private Color normal_color;
+    private Color underwater_color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isUnderwater = false;
+        normal_color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        underwater_color = new Color(0.22f, 0.65f, 0.77f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position.y < waterlevel) != isUnderwater){
+            isUnderwater = transform.position.y < waterlevel;
+            if (isUnderwater) setUnderWater();
+            if (!isUnderwater) setNormal();
+        }
+    }
+    void setNormal()
+    {
+        Debug.Log("Normal");
+        RenderSettings.fogColor = normal_color;
+        RenderSettings.fogDensity = 0.22f;
+    }
+
+    void setUnderWater()
+    {
+        Debug.Log("Underwater");
+        RenderSettings.fogColor = normal_color;
+        RenderSettings.fogDensity = 0.222f;
+    }
+}
---
+++ CityVoltexAssetTest/Assets/editSlider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class editSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public SteamVR_Action_Boolean grabPinchAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
+    public SteamVR_Action_Boolean sliderEdit;
+    public SteamVR_Action_Vector2 touchPadAction;
+    public GameObject c_obj;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (SteamVR_Input._default.inAtions.newaction.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            Debug.Log("AL");
+        }*/
+        if (sliderEdit.GetLastState(SteamVR_Input_Sources.Any))
+        {
+            print("ZZZ");
+            c_obj.transform.position = new Vector3(c_obj.transform.position.x, c_obj.transform.position.y + 1, c_obj.transform.position.z);
+        }
+        else
+        {
+            print("AAA");
+        }
+        Vector2 touchpadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+        print(touchpadValue);
+        if (touchpadValue[0] > 0)
+        {
+            c_obj.transform.position = new Vector3(c_obj.transform.position.x, c_obj.transform.position.y - 1, c_obj.transform.position.z);
+        }
+        else
+        {
+            //c_obj.transform.position = new Vector3(c_obj.transform.position.x, c_obj.transform.position.y , c_obj.transform.position.z);
+        }
+    }
+}
